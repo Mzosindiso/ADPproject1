646e000f684297e114853751b81088bdbc0fe7da
Lutho Nondala <219369399@mycput.ac.za>
2021-04-01 13:55:59 +0200

    Second Commit: Coding Operators

--- src/main/java/za/ac/cput/adpproject1/Count.java
+++ src/main/java/za/ac/cput/adpproject1/Count.java
@@ -25,6 +25,18 @@ public class Count {
         choice = sc.nextInt();
         return choice;
     }
+    public static double division(int num1, int num2){
+        return num1 / num2;
+    }
+    public static int multiplication(int num1, int num2){
+        return num1 * num2;
+    }
+    public static int addition(int num1, int num2){
+        return num1 + num2;
+    }
+    public static int subtraction(int num1, int num2){
+        return num1 - num2;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Would you like to use a caculator(Enter Y/N): ");
