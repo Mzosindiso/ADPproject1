783ddc72eabed4f95ebe133e7db82721cb5d157b
Lutho Nondala <219369399@mycput.ac.za>
2021-04-01 13:45:39 +0200

    First Commit: Operator choices

--- src/main/java/za/ac/cput/adpproject1/Count.java
+++ src/main/java/za/ac/cput/adpproject1/Count.java
@@ -5,10 +5,36 @@
  */
 package za.ac.cput.adpproject1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author mzura
  */
 public class Count {
-    
+    public static int choice(){
+        Scanner sc = new Scanner(System.in);
+        int choice;
+        System.out.println("Calculator: ");
+        System.out.println("1: Division");
+        System.out.println("2: Multiplication");
+        System.out.println("3: Addition");
+        System.out.println("4: Subtraction");
+        System.out.println(" ");
+        System.out.println("Enter the number of the operatot that you want use: ");
+        choice = sc.nextInt();
+        return choice;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Would you like to use a caculator(Enter Y/N): ");
+        char option = Character.toUpperCase(sc.nextLine().charAt(0));
+        
+        if (option == 'Y'){
+            choice();
+        }else{
+            System.out.println("You do not want to use a calculator.Proceed!");
+        }
+        
+    }
 }
