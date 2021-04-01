dcc900ac46fc0ee68ae9202fb664ed3058f6111a
Siphesonke Notsela <219076235>
2021-04-01 13:53:02 +0200

    First commit

--- src/main/java/za/ac/cput/adpproject1/Count.java
+++ src/main/java/za/ac/cput/adpproject1/Count.java
@@ -7,8 +7,42 @@ package za.ac.cput.adpproject1;
 
 /**
  *
- * @author mzura
+ * @author Siphesonke Notsela 219076235
+ *
  */
 public class Count {
     
+private String inGameName, gamerId, loginName;
+
+    public void setInGameName(String inGameName) {
+        this.inGameName = inGameName;
+    }
+
+    public void setGamerId(String gamerId) {
+        this.gamerId = gamerId;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getInGameName() {
+        return inGameName;
+    }
+
+    public String getGamerId() {
+        return gamerId;
+    }
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" + "inGameName=" + inGameName + ", gamerId=" + gamerId + ", loginName=" + loginName + '}';
+    }
+    
 }
+
+
--- src/test/java/za/ac/cput/adpproject1/CountTest.java
+++ src/test/java/za/ac/cput/adpproject1/CountTest.java
@@ -5,42 +5,77 @@
  */
 package za.ac.cput.adpproject1;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  *
- * @author mzura
+ * @author Siphesonke Notsela
+ * Student No. 219076235
  */
 public class CountTest {
     
-    public CountTest() {
-    }
+    private static Count account1;  
+    private static Count account2;  
+    private static Count account3;  
     
     @BeforeAll
-    public static void setUpClass() {
+    public static void setUp() {
+        account1 = new Count();
+        account2 = account1;
+        account3 = account2;
     }
     
-    @AfterAll
-    public static void tearDownClass() {
+    @Test
+    public void testIdentity (){
+       assertSame(account1, account3); 
     }
     
-    @BeforeEach
-    public void setUp() {
-    }
     
-    @AfterEach
-    public void tearDown() {
+    @Test
+    public void testEquality (){
+       assertEquals(account2, account3); 
     }
 
+    /**
+     * Test of setInGameName method, of class Account.
+     */
     @Test
-    public void testSomeMethod() {
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testSetInGameName() {
+        System.out.println("setInGameName");
+        String expResult = "CipherX";
+        account1.setInGameName("CipherX");
+        String result = account1.getInGameName();
+        assertSame(expResult, result);
+        fail("The test will fail.");
+    }
+
+    /**
+     * Test of setGamerId method, of class Account.
+     */
+    @Test
+    @Disabled
+    public void testSetGamerId() {
+        System.out.println("setGamerId");
+        String expResult = "204890";
+        account2.setGamerId("204890");
+        String result = account2.getGamerId();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setLoginName method, of class Account.
+     */
+    @Test
+    @Timeout(value = 2, unit = TimeUnit.SECONDS)
+    public void testSetLoginName() { 
+      System.out.println("setLonginName");
+        String expResult = "XPhoenix";
+        account1.setLoginName("XPhoenix");
+        String result = account1.getLoginName();
+        assertSame(expResult, result);
     }
     
 }
